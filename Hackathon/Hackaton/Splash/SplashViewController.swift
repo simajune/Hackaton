d4863847d8263a4e06a6256dd4f439b2f5a972c5
ssy828 <828yss@gmail.com>
2017-11-25 21:42:32 +0900

    install pod SwiftKeychainWrapper

--- Hackathon/Hackaton/Splash/SplashViewController.swift
+++ Hackathon/Hackaton/Splash/SplashViewController.swift
@@ -2,44 +2,63 @@
 import UIKit
 import Firebase
 import RevealingSplashView
+import SwiftKeychainWrapper
 
 class SplashViewController: UIViewController {
-
+    
     @IBOutlet var BG: UIImageView!
     @IBOutlet weak var logoImage: UIImageView!
+    let userKey = "username"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("splash")
+        //        print("splash")
         logoImage.alpha = 0
         
-        UIView.animate(withDuration: 2.0, animations: {
-            self.logoImage.alpha = 1.0
-        }) { (action) in
-            if let _ = Auth.auth().currentUser {
-                let mainSB = UIStoryboard(name: "Main", bundle: nil)
-                if let mainVC = mainSB.instantiateViewController(withIdentifier: "Main") as? UINavigationController {
-                    self.present(mainVC, animated: true, completion: nil)
+        //        UIView.animate(withDuration: 2.0, animations: {
+        //            self.logoImage.alpha = 1.0
+        //        }) { [weak self]_ in
+        //            guard let `self` = self else{ return }
+        //            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: {
+        //                if let keyData = KeychainWrapper.standard.data(forKey: self.userKey), let account = try? JSONDecoder().decode(Account.self, from: keyData){
+        //                    Auth.auth().signIn(withEmail: account.email, password: account.password, completion: { (user, error) in
+        //                        if error == nil, user != nil{
+        //                            self.performSegue(withIdentifier: "segueSplashToMain", sender: nil)
+        //                        }else{
+        //                            KeychainWrapper.standard.removeObject(forKey: self.userKey)
+        //                            self.performSegue(withIdentifier: "segueSplashToLogin", sender: nil)
+        //                        }
+        //                    })
+        //                }else{
+        //                    self.performSegue(withIdentifier: "segueSplashToLogin", sender: nil)
+        //                }
+        //
+        //            })
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            UIView.animate(withDuration: 2.0, animations: {
+                self.logoImage.alpha = 1.0
+            }, completion: { [weak self] _ in
+                guard let `self` = self else{ return }
+                if let keyData = KeychainWrapper.standard.data(forKey: self.userKey), let account = try? JSONDecoder().decode(Account.self, from: keyData){
+                    Auth.auth().signIn(withEmail: account.email, password: account.password, completion: { (user, error) in
+                        if error == nil, user != nil{
+                            self.performSegue(withIdentifier: "segueSplashToMain", sender: nil)
+                        }else{
+                            KeychainWrapper.standard.removeObject(forKey: self.userKey)
+                            self.performSegue(withIdentifier: "segueSplashToLogin", sender: nil)
+                        }
+                    })
+                }else{
+                    self.performSegue(withIdentifier: "segueSplashToLogin", sender: nil)
                 }
-            }else {
-                let loginSB = UIStoryboard(name: "Login", bundle: nil)
-                if let loginVC = loginSB.instantiateViewController(withIdentifier: "Login") as? UINavigationController {
-                    self.present(loginVC, animated: true, completion: nil)
-                }
-            }
-            
+            })
         }
-
-    }
-
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-//        let LoginSB = UIStoryboard(name: "Login", bundle: nil)
-//        if let LoginVC = LoginSB.instantiateViewController(withIdentifier: "Login") as? UINavigationController {
-//            self.present(LoginVC, animated: true, completion: nil)
-//        }
-
     }
 }
+
+struct Account: Decodable{
+    var email: String
+    var password: String
+}
+
+
